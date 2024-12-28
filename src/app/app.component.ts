92039ed65f3454aa18b88cda16e49ac09e356d68
Edison T T <edison.thomas795@gmail.com>
2024-12-28 22:29:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-19-demo';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.title = 'new titile';
+    }, 3_000);
+  }
 }
