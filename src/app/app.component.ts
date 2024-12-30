55ffa6e4383874a7c7c3957158c2ee54a995217c
Edison T T <edison.thomas795@gmail.com>
2024-12-30 17:24:41 +0530

    adding home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'angular-19-demo';
+  title = 'Resume';
 
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.title = 'new titile';
-    }, 3_000);
-  }
+  ngOnInit(): void {}
 }
